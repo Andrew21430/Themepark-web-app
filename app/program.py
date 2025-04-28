8a8dfe40b28682a7d8068c3a5cce2a7af1c70dab
Andrew21430 <21430@burnside.school.nz>
2025-04-28 14:23:21 +1200

    basic sqlalchemy started still models.py to be done

---
+++ app/program.py
@@ -0,0 +1,12 @@
+from app import app
+from flask import render_template, abort
+from flask_sqlalchemy import SQLAlchemy  # no more boring old SQL for us!
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, "database.db")
+db.init_app(app)
+
+import app.models as models
